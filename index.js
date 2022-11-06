9998b6d7e98d16ece5c8970239d5dc3e83b610c9
jeongsehun <tpgns933@gmail.com>
2022-11-06 10:38:35 +0900

    test

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const users = require('./fake-data/users');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
